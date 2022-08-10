221d9fcb69145659c4240b4a820958d9ce3b4f74
Fortexs <nanda.pertama465@gmail.com>
2022-08-10 20:48:03 +0700

    kosong

---
+++ Pagination/Paginations/Models/UserModel.js
@@ -0,0 +1,17 @@
+import sequelize from "sequelize";
+import db from "../Config/database";
+
+const {DataTypes} = sequelize;
+
+const Page = db.define('pagination', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    gender: DataTypes.STRING
+},{
+    freezeTableName:true
+});
+export default Page;
+
+(async()=>{
+    await Users.sync();
+})();
